519f8ec4bec4523ccb025e4c2094c3f073b33074
ZuzlifatulAdnan <juslifatuladnan@gmail.com>
2025-07-09 00:08:30 +0700

    auth dan migration tahap 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/beranda');
+
+Route::middleware(['auth'])->group(function () {
+    // beranda
+    Route::resource('beranda', BerandaController::class);
+    
+    Route::resource('user', UserController::class);
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('/profile/update/{user}', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('profile/change-password', [ProfileController::class, 'changePasswordForm'])->name('profile.change-password-form');
+    Route::post('profile/change-password/{user}', [ProfileController::class, 'changePassword'])->name('profile.change-password');
 });
